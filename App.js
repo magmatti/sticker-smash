cd8c7a16a747e8b84da15deb88a2c3b3d2b398ba
magmatt <matiwojt@gmail.com>
2024-09-17 00:36:04 +0200

    add functionality: sticker picker carousel, chosing sticker

--- App.js
+++ App.js
@@ -6,11 +6,19 @@ import * as ImagePicker from 'expo-image-picker';
 
 import ImageViewer from './components/ImageViewer';
 import Button from './components/Button';
+import CircleButton from './components/CircleButton';
+import IconButton from './components/IconButton';
+import EmojiPicker from './components/EmojiPicker';
+import EmojiList from './components/EmojiList';
+import EmojiSticker from './components/EmojiSticker';
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+  const [pickedEmoji, setPickedEmoji] = useState(null);
   const [selectedImage, setSelectedImage] = useState(null);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [showAppOptions, setShowAppOptions] = useState(false);
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -20,23 +28,55 @@ export default function App() {
 
     if (!result.canceled) {
       setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
     } else {
       alert('You did not select any photo');
     }
   };
 
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+
+  const onAddSticker = () => {
+    setIsModalVisible(true);
+  };
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  }
+
+  const onSaveImageAsync = async () => {
+    // implementation later
+  };
+
   return (
     <View style={styles.container}>
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
+      </EmojiPicker>
       <View style={styles.imageContainer}>
         <ImageViewer
           placeholderImageSource={PlaceholderImage} 
-          selectedImage={selectedImage}
-        />
+          selectedImage={selectedImage} />
+          {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji}/>}
       </View>
-      <View style={styles.footerContainer}>
-        <Button label={'Choose a photo'} theme="primary" onPress={pickImageAsync} />
-        <Button label={'Use this photo'} />
-      </View>
+      
+      {showAppOptions ? (
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="Reset" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+          </View>
+        </View>
+      ) : (
+        <View style={styles.footerContainer}>
+          <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+          <Button label="Use this photo" onPress={() => setShowAppOptions(true)} />
+        </View>
+      )}
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -61,5 +101,13 @@ const styles = StyleSheet.create({
   footerContainer: {
     flex: 1 / 3,
     alignItems: 'center',
+  },
+  optionsContainer: {
+    position: 'absolute',
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
   },
 });
---
+++ components/EmojiSticker.js
@@ -0,0 +1,13 @@
+import { View, Image } from "react-native";
+
+export default function EmojiSticker({ imageSize, stickerSource }) {
+    return (
+        <View style={{ top: -350 }}>
+            <Image
+                source={stickerSource}
+                resizeMethod="contain"
+                style={{ width: imageSize, height: imageSize }}
+            />
+        </View>
+    )
+}
